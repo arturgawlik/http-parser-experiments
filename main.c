576371703601d8cf199b0f5453ba0e11f82ea567
arturgawlik <artur.gawlik@icloud.com>
2025-04-23 23:36:00 +0200

    imprv printf

--- main.c
+++ main.c
@@ -2,11 +2,6 @@
 #include "stdio.h"
 #include "string.h"
 
-int handle_on_message_complete(http_parser *parser) {
-  fprintf(stdout, "Message completed!\n");
-  return 0;
-}
-
 int on_url(http_parser *parser, const char *at, size_t length) {
   fprintf(stdout, "on_url callback: '%s' with length '%zu' \n", at, length);
   return 0;
@@ -20,7 +15,7 @@ int main() {
   http_parser_settings_init(&settings);
 
   /*Set user callback */
-  settings.on_message_complete = handle_on_message_complete;
+  // settings.on_message_complete = handle_on_message_complete;
   settings.on_url = on_url;
 
   /*Initialize the parser in HTTP_BOTH mode, meaning that it will select between
